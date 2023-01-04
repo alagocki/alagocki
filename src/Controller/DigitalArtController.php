b92553bed10a90621e08aab7aa4ae596b8139b14
andreaslagocki <andreas@lagocki.de>
2023-01-04 09:05:27 +0100

    04.01.2023

---
+++ src/Controller/DigitalArtController.php
@@ -0,0 +1,30 @@
+<?php
+/*
+ * Andreas Lagocki - Photographer
+ *
+ * Copyright (C) Andreas Lagocki  - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly
+ *  prohibited Proprietary and confidential.
+ *
+ * @author Andreas Lagocki <andreas@lagocki.de>
+ */
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('')]
+class DigitalArtController extends AbstractController
+{
+    #[Route('/fotowelt', name: 'fotowelt')]
+    public function dashboard(): Response
+    {
+        $twigVars = [
+            'user' => $this->getUser(),
+        ];
+
+        return $this->render('admin/digital-art.html.twig', $twigVars);
+    }
+}
